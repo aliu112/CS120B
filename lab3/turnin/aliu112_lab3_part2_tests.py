8cc623414918387098de495c9fe212deafbe8c09
aliu112 <aliu112@ucr.edu>
2021-10-08 22:51:32 -0700

    submited wrong test file

--- lab3/turnin/aliu112_lab3_part2_tests.py
+++ lab3/turnin/aliu112_lab3_part2_tests.py
@@ -1,46 +1,39 @@
-# Test file for "lab3"
+# Array of tests to run (in order)
+# Each test contains
+#   description - 
+#   steps - A list of steps to perform, each step can have
+#       inputs - A list of tuples for the inputs to apply at that step
+#       *time - The time (in ms) to wait before continuing to the next step 
+#           and before checking expected values for this step. The time should be a multiple of
+#           the period of the system
+#       *iterations - The number of clock ticks to wait (periods)
+#       expected - The expected value at the end of this step (after the "time" has elapsed.) 
+#           If this value is incorrect the test will fail early before completing.
+#       * only one of these should be used
+#   expected - The expected output (as a list of tuples) at the end of this test
+# An example set of tests is shown below. It is important to note that these tests are not "unit tests" in 
+# that they are not ran in isolation but in the order shown and the state of the device is not reset or 
+# altered in between executions (unless preconditions are us
 
+# Optionally you can add a set of "watch" variables these need to be global or static and may need
+# to be scoped at the function level (for static variables) if there are naming conflicts. The 
+# variables listed here will display everytime you hit (and stop at) a breakpoint
+tests = [ {'description': 'PINA: 0x08,PINC =0x3A ',
+    'steps': [ {'inputs': [('PINA',0x08)], 'iterations': 5 } ],
+    'expected': [('PORTC', 0x3C)],
+    },
 
-# commands.gdb provides the following functions for ease:
-#   test "<message>"
-#       Where <message> is the message to print. Must call this at the beginning of every test
-#       Example: test "PINA: 0x00 => expect PORTC: 0x01"
-#   checkResult
-#       Verify if the test passed or failed. Prints "passed." or "failed." accordingly, 
-#       Must call this at the end of every test.
-#   expectPORTx <val>
-#       With x as the port (A,B,C,D)
-#       The value the port is epected to have. If not it will print the erroneous actual value
-#   setPINx <val>
-#       With x as the port or pin (A,B,C,D)
-#       The value to set the pin to (can be decimal or hexidecimal
-#       Example: setPINA 0x01
-#   printPORTx f OR printPINx f 
-#       With x as the port or pin (A,B,C,D)
-#       With f as a format option which can be: [d] decimal, [x] hexadecmial (default), [t] binary 
-#       Example: printPORTC d
-#   printDDRx
-#       With x as the DDR (A,B,C,D)
-#       Example: printDDRB
+    {'description': 'PINA: 0x04, PINC =0x70',
+    'steps': [ {'inputs': [('PINA',0x04)], 'iterations': 5 } ],
+    'expected': [('PORTC', 0x70)],
+    },
+    {'description': 'PINA: 0x0F => PORTc: 0x1F',
+    'steps': [ {'inputs': [('PINA',0x0F)], 'iterations': 5 } ],
+    'expected': [('PORTC',0x3F)],
+    },
+{'description': 'PINA: 0x5 => PORTC: 0x38',
+    'steps': [ {'inputs': [('PINA',0x05)], 'iterations': 5 } ],
+    'expected': [('PORTC',0x38)],
+    },
 
-echo ======================================================\n
-echo Running all tests..."\n\n
-
-# Example test:
-test "PINA: 0x00, PINB: 0x00 => PORTC: 0"
-# Set inputs
-setPINA 0x00
-setPINB 0x00
-# Continue for several ticks
-continue 2
-# Set expect values
-expectPORTC 0
-# Check pass/fail
-checkResult
-
-# Add tests below
-
-# Report on how many tests passed/tests ran
-set $passed=$tests-$failed
-eval "shell echo Passed %d/%d tests.\n",$passed,$tests
-echo ======================================================\n
+    ]
