9676cdfef3ad7e442b081aa06e264b8a19f6b0a4
aliu112 <aliu112@ucr.edu>
2021-10-09 00:58:25 -0700

    Completed part 5

---
+++ lab3/turnin/aliu112_lab3_part5.c
@@ -0,0 +1,49 @@
+/*	Author: lab
+ *  Partner(s) Name: none
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+    DDRD = 0x00; PORTD = 0xFF;    
+    DDRB = 0xFE; PORTB = 0x00;
+    
+    
+    /* Insert your solution below */
+    unsigned char temp = 0x000;
+    unsigned char tempNew = 0x000;
+    unsigned char output = 0x00;
+
+    while (1) {
+	output = 0x00;
+	temp = PIND; 
+	tempNew = PINB;
+	tempNew = tempNew & 0x001;
+        temp = temp << 1;
+	temp = temp | tempNew;
+
+	if(temp >= 70)
+	{
+		output = output | 0x02;
+	}
+	if(temp> 5 && temp <70)
+	{
+		output = output | 0x04;
+	}
+	PORTB = output;
+		
+    
+    
+    }
+    	return 1;
+    
+  }
